1df1d41b0ec22626b695dd54c842f072f7c47f0e
David Corson <david.l.corson@gmail.com>
2021-06-16 23:25:17 -0400

    removing edit button for topic component

--- lib/metapede/collection/topic.ex
+++ lib/metapede/collection/topic.ex
@@ -38,9 +38,9 @@ defmodule Metapede.Collection.Topic do
 
   def changeset(struct, params \\ %{}) do
     struct
-      |> Ecto.Changeset.cast(params, @accepted_fields)
-      |> Ecto.Changeset.validate_required([:title, :description])
-      |> Ecto.Changeset.foreign_key_constraint(:sub_topics)
-      |> Ecto.Changeset.foreign_key_constraint(:parent_topics)
+    |> Ecto.Changeset.cast(params, @accepted_fields)
+    |> Ecto.Changeset.validate_required([:title])
+    |> Ecto.Changeset.foreign_key_constraint(:sub_topics)
+    |> Ecto.Changeset.foreign_key_constraint(:parent_topics)
   end
 end
--- lib/metapede_web/live/live_components/topic/topic_component.ex
+++ lib/metapede_web/live/live_components/topic/topic_component.ex
@@ -1,6 +1,5 @@
 defmodule MetapedeWeb.LiveComponents.Topic.TopicComponent do
   use MetapedeWeb, :live_component
-  alias Metapede.Collection
 
   def render(assigns) do
     ~L"""
@@ -9,7 +8,6 @@ defmodule MetapedeWeb.LiveComponents.Topic.TopicComponent do
         <div><%= @topic.description %></div>
         <div>
             <span><%= live_redirect "Show", to: Routes.topic_show_path(@socket, :show, @topic) %></span>
-            <span><%= live_patch "Edit", to: Routes.topic_topics_path(@socket, :edit, @topic) %></span>
             <span><%= link "Delete", to: "#", phx_click: "delete" , phx_value_id: @topic.id, data: [confirm: "Are you sure?"] %></span>
         </div>
     </div>
