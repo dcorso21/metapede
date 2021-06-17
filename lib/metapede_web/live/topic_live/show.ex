cc7624731b7b81c3f84ba69d3d180ce5b0ce5806
David Corson <david.l.corson@gmail.com>
2021-06-16 22:54:31 -0400

    adding existing subtopics

--- lib/metapede_web/live/topic_live/show.ex
+++ lib/metapede_web/live/topic_live/show.ex
@@ -14,40 +14,38 @@ defmodule MetapedeWeb.TopicLive.Show do
   end
 
   @impl true
-  def handle_event("send_topic", %{"data" => selected_topic}, socket) do
-    new_sub_topic =
-      Poison.decode!(selected_topic)
-      |> WikiTransforms.transform_wiki_data()
-
-    existing_ids = Collection.check_for_page_id(new_sub_topic["page_id"])
-
-    case existing_ids do
-      [] ->
-        IO.puts("New")
-        test_add(new_sub_topic, socket)
-
-      [_id] ->
-        IO.puts("Existing")
-        test_add(new_sub_topic, socket)
-    end
-  end
-
   def handle_event("delete", %{"id" => id}, socket) do
     topic = Collection.get_topic!(id)
     {:ok, _} = Collection.delete_topic(topic)
     {:noreply, assign(socket, :topic, Collection.get_topic!(socket.assigns.topic.id))}
   end
 
-  defp test_add(new_topic, socket) do
-    IO.puts("New Topic")
-    IO.puts(inspect(new_topic))
+  def handle_event("send_topic", %{"data" => selected_topic}, socket) do
+    sub_topic =
+      Poison.decode!(selected_topic)
+      |> WikiTransforms.transform_wiki_data()
 
-    new_sub_topic = Topic.changeset(%Topic{}, new_topic)
+    existing_ids = Collection.check_for_page_id(sub_topic["page_id"])
+
+    case existing_ids do
+      [] ->
+        IO.puts("New")
+        params = get_sub_topic(sub_topic, nil, :new)
+        test_add(params, socket)
+
+      [id] ->
+        IO.puts("Existing")
+        params = get_sub_topic(nil, id, :existing)
+        test_add(params, socket)
+    end
+  end
+
+  defp test_add(new_topic, socket) do
     parent_topic = socket.assigns.topic
 
     parent_topic
     |> Ecto.Changeset.change()
-    |> Ecto.Changeset.put_assoc(:sub_topics, [new_sub_topic | parent_topic.sub_topics])
+    |> Ecto.Changeset.put_assoc(:sub_topics, [new_topic | parent_topic.sub_topics])
     |> Repo.update!()
 
     {:noreply,
@@ -55,6 +53,9 @@ defmodule MetapedeWeb.TopicLive.Show do
      |> put_flash(:info, "Subtopic Added")
      |> push_redirect(to: Routes.topic_show_path(socket, :show, socket.assigns.topic))}
   end
+
+  defp get_sub_topic(_params, id, :existing), do: Collection.get_topic!(id)
+  defp get_sub_topic(params, _id, :new), do: Topic.changeset(%Topic{}, params)
 
   defp page_title(:show), do: "Show Topic"
   defp page_title(:edit), do: "Edit Topic"
