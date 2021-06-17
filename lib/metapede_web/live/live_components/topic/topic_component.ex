e10704a03238b889f91ca138a450a5df36e89943
David Corson <david.l.corson@gmail.com>
2021-06-16 22:29:02 -0400

    deleting sub topics

--- lib/metapede_web/live/live_components/topic/topic_component.ex
+++ lib/metapede_web/live/live_components/topic/topic_component.ex
@@ -1,5 +1,6 @@
 defmodule MetapedeWeb.LiveComponents.Topic.TopicComponent do
   use MetapedeWeb, :live_component
+  alias Metapede.Collection
 
   def render(assigns) do
     ~L"""
@@ -9,7 +10,7 @@ defmodule MetapedeWeb.LiveComponents.Topic.TopicComponent do
         <div>
             <span><%= live_redirect "Show", to: Routes.topic_show_path(@socket, :show, @topic) %></span>
             <span><%= live_patch "Edit", to: Routes.topic_topics_path(@socket, :edit, @topic) %></span>
-            <span><%= link "Delete", to: "#", phx_click: "delete", phx_value_id: @topic.id, data: [confirm: "Are you sure?"] %></span>
+            <span><%= link "Delete", to: "#", phx_click: "delete" , phx_value_id: @topic.id, data: [confirm: "Are you sure?"] %></span>
         </div>
     </div>
     """
--- lib/metapede_web/live/topic_live/show.ex
+++ lib/metapede_web/live/topic_live/show.ex
@@ -33,23 +33,29 @@ defmodule MetapedeWeb.TopicLive.Show do
     end
   end
 
+  def handle_event("delete", %{"id" => id}, socket) do
+    topic = Collection.get_topic!(id)
+    {:ok, _} = Collection.delete_topic(topic)
+    {:noreply, assign(socket, :topic, Collection.get_topic!(id))}
+  end
+
   defp test_add(new_topic, socket) do
     IO.puts("New Topic")
     IO.puts(inspect(new_topic))
 
-    new_sub_topic =  Topic.changeset(%Topic{}, new_topic)
-    parent_topic =socket.assigns.topic
+    new_sub_topic = Topic.changeset(%Topic{}, new_topic)
+    parent_topic = socket.assigns.topic
 
     parent_topic
-      |> Ecto.Changeset.change()
-      |> Ecto.Changeset.put_assoc(:sub_topics, [new_sub_topic | parent_topic.sub_topics])
-      |> Repo.update!()
+    |> Ecto.Changeset.change()
+    |> Ecto.Changeset.put_assoc(:sub_topics, [new_sub_topic | parent_topic.sub_topics])
+    |> Repo.update!()
 
-    # IO.puts(inspect(res))
-    # Collection.add_subtopic(res)
-    {:noreply, socket}
+    {:noreply,
+     socket
+     |> put_flash(:info, "Subtopic Added")
+     |> push_redirect(to: Routes.topic_show_path(socket, :show, socket.assigns.topic))}
   end
-
 
   defp page_title(:show), do: "Show Topic"
   defp page_title(:edit), do: "Edit Topic"
