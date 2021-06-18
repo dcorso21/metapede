e07a124144033e35988a47d6a0474b0189aad965
David Corson <david.l.corson@gmail.com>
2021-06-17 23:23:08 -0400

    page load on button click

--- lib/metapede_web/live/live_components/wiki_content.ex
+++ lib/metapede_web/live/live_components/wiki_content.ex
@@ -1,10 +1,18 @@
 defmodule MetapedeWeb.LiveComponents.WikiContent do
   use MetapedeWeb, :live_component
 
+  def update(assigns, socket) do
+    socket =
+        socket
+        |> assign(:wiki_content, nil)
+        |> assign(:page_id, assigns.page_id)
+    {:ok, socket}
+  end
+
   def render(assigns) do
     ~L"""
     <div>
-    <button phx-click="loadpage" phx-target="<%= @myself %>"></button>
+    <button phx-click="loadpage" phx-target="<%= @myself %>">Load Page</button>
         <%= if @wiki_content do %>
             <%= raw(@wiki_content) %>
         <% end %>
@@ -13,7 +21,7 @@ defmodule MetapedeWeb.LiveComponents.WikiContent do
   end
 
   def handle_event("loadpage", _, socket) do
-    {:ok, assign(socket, :wiki_content, Metapede.WikiFuncs.get_page(socket.assigns.page_id))}
-    {:noreply, socket}
+    IO.puts "Hello"
+    {:noreply, assign(socket, :wiki_content, Metapede.WikiFuncs.get_page(socket.assigns.page_id))}
   end
 end
