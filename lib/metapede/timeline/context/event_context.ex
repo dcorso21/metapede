f5bb09fc3dbaab00149e7eee516775dd031b2736
David Corson <david.l.corson@gmail.com>
2021-06-19 21:29:52 -0400

    refactor, add preload for parent time_periods to event

---
+++ lib/metapede/timeline/context/event_context.ex
@@ -0,0 +1,35 @@
+defmodule Metapede.TimelineContext.EventContext do
+
+  import Ecto.Query, warn: false
+  alias Metapede.Repo
+  alias Metapede.Timeline.{Event}
+  @preload [:topic, :parent_time_periods]
+
+  def list_events() do
+    Event
+    |> Repo.all()
+    |> Repo.preload(@preload)
+  end
+
+  def get_event!(id) do
+    Event
+    |> Repo.get!(id)
+    |> Repo.preload(@preload)
+  end
+
+  def create_event(attrs \\ %{}) do
+    %Event{}
+    |> Event.changeset(attrs)
+    |> Repo.insert()
+  end
+
+  def update_event(%Event{} = event, attrs) do
+    event
+    |> Event.changeset(attrs)
+    |> Repo.update()
+  end
+
+  def delete_event(%Event{} = event) do
+    Repo.delete(event)
+  end
+end
