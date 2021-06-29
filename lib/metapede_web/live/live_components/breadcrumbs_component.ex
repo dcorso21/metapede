3f23c4b50d33e1af2b6f6f0d1a005858367bf2e9
David Corson <david.l.corson@gmail.com>
2021-06-29 00:52:22 -0400

    styling breadcrumbs

--- lib/metapede_web/live/live_components/breadcrumbs_component.ex
+++ lib/metapede_web/live/live_components/breadcrumbs_component.ex
@@ -4,14 +4,21 @@ defmodule MetapedeWeb.LiveComponents.BreadcrumbsComponent do
 
   def render(assigns) do
     ~L"""
-    <div>
+    <div class="breadcrumbs">
+        <div class="crumb">
         <span>
-              <%= live_patch @root, to: @root_path%>
+        <%= live_patch @root, to: @root_path%>
         </span>
+        </div>
+        <div class="caret">></div>
         <%= for { crumb, index } <- Enum.with_index(Enum.reverse(@breadcrumbs)) do %>
             <%= live_component @socket, Crumb, crumb: crumb, index: index %>
         <% end %>
-        <span> > <%= @current_title %></span>
+        <div class="crumb">
+        <span>
+        <%= @current_title %>
+        </span>
+        </div>
     </div>
     """
   end
@@ -22,12 +29,15 @@ defmodule MetapedeWeb.LiveComponents.Crumb do
 
   def render(assigns) do
     ~L"""
-    <span> >
-        <%= live_patch elem(@crumb, 0),
-        to: Routes.time_period_show_path(@socket, :show, elem(@crumb, 1)),
-        phx_click: "reset_breadcrumbs" <> Integer.to_string(@index)
-        %>
+    <div class="crumb">
+    <span>
+    <%= live_patch elem(@crumb, 0),
+    to: Routes.time_period_show_path(@socket, :show, elem(@crumb, 1)),
+    phx_click: "reset_breadcrumbs" <> Integer.to_string(@index)
+    %>
     </span>
+    </div>
+    <div class="caret">></div>
     """
   end
 end
