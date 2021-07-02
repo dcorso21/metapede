c4b4bca8906557181a951014d264093554d0f614
David Corson <david.l.corson@gmail.com>
2021-07-02 16:59:55 -0400

    styling and simplifying date selector component.

--- lib/metapede_web/live/live_components/time_period/date_selector.ex
+++ lib/metapede_web/live/live_components/time_period/date_selector.ex
@@ -2,17 +2,54 @@ defmodule MetapedeWeb.LiveComponents.TimePeriod.DateSelectorComponent do
   use MetapedeWeb, :live_component
 
   def render(assigns) do
+    months = [
+      "Jan",
+      "Feb",
+      "Mar",
+      "Apr",
+      "May",
+      "Jun",
+      "Jul",
+      "Aug",
+      "Sep",
+      "Oct",
+      "Nov",
+      "Dec"
+    ]
+
     ~L"""
-    <input type="text" name="year" value=""
+    <div class="date_selector">
+    <h4 class="title"><%= @title %></h4>
+    <div class="year">
+    <input type="text" name="year"
            placeholder="Year" />
-    <select multiple size="3">
+    <select>
         <option selected>C.E.</option>
         <option>B.C.E</option>
     </select>
-    <input type="text" name="month" value=""
-           placeholder="Month" />
-    <input type="text" name="day" value=""
-           placeholder="Day" />
+    </div>
+
+    <div class="month">
+    <select name="month">
+      <option selected>Month (empty)<option>
+        <%= for month <- months do %>
+            <option><%= month %></option>
+        <% end %>
+    </select>
+    </div>
+
+    <div class="day">
+    <select name="day">
+        <option selected>Day (empty)</option>
+        <%= for day <- 1..31 do %>
+            <option> <%= day %></option>
+        <% end %>
+    </select>
+    </div>
+    </div>
+
+
+
     """
   end
 end
