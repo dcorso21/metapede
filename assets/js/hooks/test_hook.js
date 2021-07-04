9f0531e2ddbbdb156da0fffbde2ab096221f8cd0
David Corson <david.l.corson@gmail.com>
2021-07-04 15:08:19 -0400

    sub periods are now working

--- assets/js/hooks/test_hook.js
+++ assets/js/hooks/test_hook.js
@@ -2,7 +2,7 @@ import * as d3 from "d3";
 import moment from "moment";
 
 const tlFuncs = {
-    height: 30,
+    height: 60,
     delay: 30,
     transform(data) {
         if (!data.length) return data;
@@ -15,22 +15,34 @@ const tlFuncs = {
         let beginning = with_times.reduce((a, b) => {
             return a.start < b.start ? a : b;
         }).start;
+
         let ending = with_times.reduce((a, b) => {
             return a.end > b.end ? a : b;
         }).end;
 
         console.log({ beginning, ending });
 
-        return data;
+        let denom = moment(ending).diff(beginning);
+
+        let periods = with_times.map((p) => {
+            p.width = (moment(p.end).diff(p.start) / denom) * 100 + "%";
+            p.ml = (moment(p.start).diff(beginning) / denom) * 100 + "%";
+            return p;
+        });
+
+        return periods.sort((a, b) => {
+            return a.start.unix() - b.start.unix();
+        });
     },
 
     render(el) {
         let sub_periods = JSON.parse(el.dataset.sub_periods);
         let main_period = JSON.parse(el.dataset.main_period);
-        console.log({sub_periods, main_period});
+        console.log({ sub_periods, main_period });
         sub_periods = this.transform(sub_periods);
 
         d3.select(el)
+            .style("height", sub_periods.length * this.height + "px")
             .selectAll(".sub_period")
             .data(sub_periods, (d) => d.id) // ID for tracking
             .join(this.enterPeriods, this.updatePeriods, this.exitPeriods);
@@ -40,11 +52,12 @@ const tlFuncs = {
         return enter
             .append("div")
             .attr("class", "sub_period")
-            .text((d) => d.topic.title);
-
+            .text((d) => d.topic.title)
+            .style("width", (d) => d.width)
+            .style("left", (d) => d.ml)
+            .style("top", (_, i) => tlFuncs.height * i + "px")
+            .style("height", () => tlFuncs.height + "px");
         // .style("left", (d) => d.x)
-        // .style("top", (_, i) => this.height * i + "px")
-        // .style("height", () => this.height + "px")
         // .style("width", (d) => d.width)
         // .call((enter) => {
         //     enter
@@ -62,7 +75,8 @@ const tlFuncs = {
                 //     delayInd++;
                 //     return delayInd * this.delay;
                 // })
-                .style("top", (_, i) => i * this.height + "px");
+                .style("top", (_, i) => i * this.height + "px")
+                .style("width", (d) => d.width);
         });
     },
 
