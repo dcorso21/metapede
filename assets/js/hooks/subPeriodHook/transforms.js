cc23869376f025dc00e237cfef6ce6870fe39681
David Corson <david.l.corson@gmail.com>
2021-07-15 02:32:11 +0000

    new transform method for better expansion

--- assets/js/hooks/subPeriodHook/transforms.js
+++ assets/js/hooks/subPeriodHook/transforms.js
@@ -1,54 +1,62 @@
 // @ts-check
 
-
 import moment from "moment";
-
-function flatten(periods) {
-	let to_add = periods.map((p) => {
-		if (p.expand && Array.isArray(p.sub_time_periods)) {
-			return flatten(p.sub_time_periods);
-		}
-		return [];
-	});
-
-	let subs = to_add.reduce((a, b) => [...a, ...b], []);
-	return [...periods, ...subs];
-}
-
-function transform(data) {
-	if (!data.length) return data;
-
-	data = flatten(data);
-
-	let with_times = data.map((d) => {
-		d.start = moment(d.start_datetime, moment.ISO_8601);
-		d.end = moment(d.end_datetime, moment.ISO_8601);
-		return d;
-	});
-
-	let beginning = with_times.reduce((a, b) => {
-		return a.start < b.start ? a : b;
-	}).start;
-
-	let ending = with_times.reduce((a, b) => {
-		return a.end > b.end ? a : b;
-	}).end;
-
-	let denom = moment(ending).diff(beginning);
-
-	let periods = with_times.map((p) => {
-		p.width = (moment(p.end).diff(p.start) / denom) * 100 + "%";
-		p.ml = (moment(p.start).diff(beginning) / denom) * 100 + "%";
-		return p;
-	})
-
-	return periods.sort((a, b) => {
-		return a.start.unix() - b.start.unix();
-	});
-}
 
 const subPeriodTransforms = {
 	transform
 }
 
 export default subPeriodTransforms;
+
+
+function transform(periods) {
+	let flattened = [];
+
+	let startTimes = [];
+	let endTimes = [];
+
+	makeRecords(periods);
+
+	function sortByDateTime(periods) {
+		let with_times = periods.map((d) => {
+			d.start = moment(d.start_datetime, moment.ISO_8601).unix();
+			d.end = moment(d.end_datetime, moment.ISO_8601).unix();
+
+			startTimes.push(d.start)
+			endTimes.push(d.end)
+			return d;
+		});
+
+		return with_times.sort((a, b) => {
+			return a.start - b.start;
+		});
+	}
+
+	function makeRecords(periods) {
+		let sorted = sortByDateTime(periods)
+		sorted.map(p => {
+			flattened.push(p)
+			if (p.expand && Array.isArray(p.sub_time_periods)) {
+				makeRecords(p.sub_time_periods)
+			}
+		})
+	}
+
+	let beginning = Math.min(...startTimes)
+	let ending = Math.max(...endTimes)
+	let denom = moment(ending).diff(beginning);
+	return flattened.map(p => {
+		p.width = (moment(p.end).diff(p.start) / denom) * 100 + "%";
+		p.ml = (moment(p.start).diff(beginning) / denom) * 100 + "%";
+		p.sub_time_periods = countSubPeriods(p)
+
+		return p;
+	})
+}
+
+function countSubPeriods(period) {
+	const stps = period.sub_time_periods;
+	const loaded = Array.isArray(stps)
+	if (loaded && period.expand) return stps.length;
+	return 0;
+}
