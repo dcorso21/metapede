d5921bbf31e4a0fa9a01274d67941c7a25945c2b
David Corson <53447317+dcorso21@users.noreply.github.com>
2021-07-19 05:08:39 +0000

    start layout for right info

---
+++ lib/metapede_web/live/live_components/expand_info.ex
@@ -0,0 +1,11 @@
+defmodule MetapedeWeb.LiveComponents.ExpandInfo do
+  use MetapedeWeb, :live_component
+
+  def render(assigns) do
+    ~L"""
+    <div>
+        Hello There
+    </div>
+    """
+  end
+end
