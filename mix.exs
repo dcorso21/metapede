367f7851f90429ebc955f157ff3baaff0f8d5413
David Corson <david.l.corson@gmail.com>
2021-08-07 17:43:39 -0400

    set up mongo

--- mix.exs
+++ mix.exs
@@ -48,7 +48,8 @@ defmodule Metapede.MixProject do
       {:jason, "~> 1.0"},
       {:plug_cowboy, "~> 2.0"},
       {:httpoison, "~> 1.8"},
-      {:poison, "~> 3.1"}
+      {:poison, "~> 3.1"},
+      {:mongodb, "~> 0.5.1"}
     ]
   end
 
